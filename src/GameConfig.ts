645fba714d7323bb402b904e7d358d0f63406415
The0Show <62229104+The0Show@users.noreply.github.com>
2023-09-22 13:45:07 -0400

    initial

---
+++ src/GameConfig.ts
@@ -0,0 +1,19 @@
+interface GameConfig {
+    JUMP_VELOCITY: number;
+    JUMP_GRAVITY: number;
+    MAX_GAME_X_SPEED: number;
+    SPEED_BOOST: number;
+    SPEED_FACTOR: number;
+    SPAWN_STOP_TIME: number;
+    PLAYER_STOP_TIME: number;
+    STAR_POINTS: number;
+    SUPERSTAR_POINTS: number;
+    IMPERVIOUS_TIME: number;
+    MULTIPLIER_TIME: number;
+    SPEED_BOOST_TIME: number;
+    
+}
+
+export const defaultGameConfig = {
+
+}
